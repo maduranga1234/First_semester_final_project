46188b060045af7728e7a145645ea313845bfbc2
unknown <valkonx007>
2023-03-15 23:28:23 +0530

    second commit

--- src/main/java/lk/ijse/super_cargo/controller/DashBordController.java
+++ src/main/java/lk/ijse/super_cargo/controller/DashBordController.java
@@ -5,10 +5,10 @@ import com.jfoenix.controls.JFXButton;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 
 public class DashBordController {
@@ -26,10 +26,11 @@ public class DashBordController {
     private JFXButton stockBtn;
 
     @FXML
-    void stockBtnClick(MouseEvent event) throws IOException {
+    void stockBtnClick(ActionEvent event) throws IOException {
         Parent load = FXMLLoader.load(getClass().getResource("/lk.ijse.super_cargo.view/stockss.fxml"));
         AnchorpaneHome.getChildren().clear();
         AnchorpaneHome.getChildren().add(load);
+
     }
 
     @FXML
--- src/main/java/lk/ijse/super_cargo/controller/StocksController.java
+++ src/main/java/lk/ijse/super_cargo/controller/StocksController.java
@@ -1,11 +1,8 @@
 package lk.ijse.super_cargo.controller;
 
-import com.jfoenix.controls.JFXButton;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.layout.AnchorPane;
 
 public class StocksController {
 
@@ -16,20 +13,7 @@ public class StocksController {
     private URL location;
 
     @FXML
-    private AnchorPane AnchorpaneHome;
-
-    @FXML
-    private JFXButton stockBtn;
-
-    @FXML
-    void stockBtnClick(ActionEvent event) {
-
-    }
-
-    @FXML
     void initialize() {
-        assert AnchorpaneHome != null : "fx:id=\"AnchorpaneHome\" was not injected: check your FXML file 'stockss.fxml'.";
-        assert stockBtn != null : "fx:id=\"stockBtn\" was not injected: check your FXML file 'stockss.fxml'.";
 
     }
 }
